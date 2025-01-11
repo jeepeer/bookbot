fbd5c6bdc9667294cc87bab82b3b613320b30818
jeepeer <jespervski@gmail.com>
2025-01-11 15:36:40 +0100

    add word counter

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 def main():
     with open("/home/test/workspace/github.com/jeepeer/bookbot/books/frankenstein.txt") as f:
         file_contents = f.read()
-        print(file_contents)
+        #print(file_contents)
+        print(wordCounter(file_contents))
+
+def wordCounter(words):  
+    print(len(words.split()))
 
 main()
+
