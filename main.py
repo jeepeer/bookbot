288d8468855483f8265fb12c7d54ad6c17109228
jeepeer <jespervski@gmail.com>
2025-01-11 16:03:19 +0100

    add get_chars_dict()

--- main.py
+++ main.py
@@ -2,10 +2,21 @@ def main():
     with open("/home/test/workspace/github.com/jeepeer/bookbot/books/frankenstein.txt") as f:
         file_contents = f.read()
         #print(file_contents)
-        print(wordCounter(file_contents))
+        #print(wordCounter(file_contents))
+        print(get_chars_dict(file_contents))
 
-def wordCounter(words):  
-    print(len(words.split()))
+def get_num_words(text):  
+    print(len(text.split()))
+
+def get_chars_dict(text):
+    characters = {}
+    for char in text:
+        lower_char = char.lower()
+        if lower_char in characters:
+            characters[lower_char] += 1
+        else:
+            characters[lower_char] = 1
+
+    return characters
 
 main()
-
